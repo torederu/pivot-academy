c9f800a4309b487ecdd42f4a88d1ccc7e7ee6b14
Tem Orederu <41482770+torederu@users.noreply.github.com>
2024-10-04 11:23:19 -0400

    Update pivot_data.py

--- pivot_data.py
+++ pivot_data.py
@@ -11,13 +11,15 @@ oct_reviews = [
 
 # November Reviews
 nov_reviews = [
-    [1001, 'November', 5, 'Great product! Highly recommend.'], 
-    [1002, 'November', None, 'Good value for money. Could be better.'],
-    [1003, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
-    [1004, 'November', 1, 'Terrible quality. Completely useless after it broke.'],
-    [1009, 'November', 5, 'Amazing product! Would buy again.'],
-    [1010, 'November', 3, 'Decent performance but overpriced.'],
-    [1011, 'November', 2, 'Stopped working. Very disappointing.'],
-    [1012, 'November', None, 'Not great, but works for now.'],
-    [1006, 'November', None, 'Broke within a few days. Would not recommend.'], 
-    [1013, 'November', 1, 'Awful experience. It broke after just one use!']]
+    [1010, 'November', 5, 'Love it! Definitely recommend.'], 
+    [1011, 'November', None, 'Gets the job done. Worth the money.'],
+    [1012, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
+    [1013, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
+    [1014, 'November', 1, 'Terrible quality. Completely useless after it broke.'],
+    [1015, 'November', 5, 'Amazing product! Would buy again.'],
+    [1016, 'November', 3, 'Decent performance but overpriced.'],
+    [1017, 'November', 3, 'Decent performance but overpriced.'],
+    [1018, 'November', 2, 'Stopped working. Very disappointing.'],
+    [1019, 'November', None, 'Not great, but works for now.'],
+    [1020, 'November', None, 'Broke within a few days. Would not recommend.'], 
+    [1021, 'November', 1, 'Awful experience. It broke after just one use!']]
