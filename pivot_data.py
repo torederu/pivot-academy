f8cc61dce7e51e77700a92ca41c6ff60bbb45d9b
Tem Orederu <41482770+torederu@users.noreply.github.com>
2024-10-03 21:19:49 -0400

    Add files via upload

---
+++ pivot_data.py
@@ -0,0 +1,23 @@
+# October Reviews
+oct_reviews = [
+    [1001, 'October', 5, 'Great product! Highly recommend.'],
+    [1002, 'October', 4, 'Good value for money. Could be better.'],
+    [1003, 'October', 2, 'Stopped working after a month. Disappointed.'],
+    [1004, 'October', 1, 'Terrible quality. Broke after a week.'],
+    [1005, 'October', 3, 'Average. Nothing special.'],
+    [1006, 'October', 2, 'Not worth the price. Broke within a few days.'],
+    [1007, 'October', 5, 'Excellent quality! Very satisfied.'],
+    [1008, 'October', 4, 'Works well, but could be more durable.']]
+
+# November Reviews
+nov_reviews = [
+    [1001, 'November', 5, 'Great product! Highly recommend.'], 
+    [1002, 'November', None, 'Good value for money. Could be better.'],
+    [1003, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
+    [1004, 'November', 1, 'Terrible quality. Completely useless after it broke.'],
+    [1009, 'November', 5, 'Amazing product! Would buy again.'],
+    [1010, 'November', 3, 'Decent performance but overpriced.'],
+    [1011, 'November', 2, 'Stopped working. Very disappointing.'],
+    [1012, 'November', None, 'Not great, but works for now.'],
+    [1006, 'November', None, 'Broke within a few days. Would not recommend.'], 
+    [1013, 'November', 1, 'Awful experience. It broke after just one use!']]
