b235ed0add08ce080117a03f61bec66bd8c5e8cf
Tem Orederu <41482770+torederu@users.noreply.github.com>
2024-10-26 22:52:14 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,25 @@
+# October Reviews
+oct_reviews = [
+    [1001, 'October', 5, 'Great product! Highly recommend.'],
+    [1002, 'October', 4, 'Good value for money. Could be better.'],
+    [1003, 'October', 2, 'Stopped working after a month. Disappointed.'],
+    [1004, 'October', 1, 'Terrible quality. Broke after a week.'],
+    [1005, 'October', 3, 'Average. Nothing special.'],
+    [1006, 'October', 2, 'Not worth the price. Broke within a few days.'],
+    [1007, 'October', 5, 'Excellent quality! Very satisfied.'],
+    [1008, 'October', 4, 'Works well, but could be more durable.']]
+
+# November Reviews
+nov_reviews = [
+    [1010, 'November', 5, 'Love it! Definitely recommend.'], 
+    [1011, 'November', None, 'Gets the job done. Worth the money.'],
+    [1012, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
+    [1013, 'November', 2, 'Broke after two weeks. Definitely not satisfied.'],
+    [1014, 'November', 1, 'Terrible quality. Completely useless after it broke.'],
+    [1015, 'November', 5, 'Amazing product! Would buy again.'],
+    [1016, 'November', 3, 'Decent performance but overpriced.'],
+    [1017, 'November', 3, 'Decent performance but overpriced.'],
+    [1018, 'November', 2, 'Stopped working. Very disappointing.'],
+    [1019, 'November', None, 'Not great, but works for now.'],
+    [1020, 'November', None, 'Broke within a few days. Would not recommend.'], 
+    [1021, 'November', 1, 'Awful experience. It broke after just one use!']]
